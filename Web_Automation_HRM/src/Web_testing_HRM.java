3f27bd4002427544ef7220f424188387d88f7475
Birenbeldar <birenbeldar007@gmail.com>
2024-03-24 12:46:30 +0530

    this is it

--- Web_Automation_HRM/src/Web_testing_HRM.java
+++ Web_Automation_HRM/src/Web_testing_HRM.java
@@ -1,6 +1,6 @@
-
-
-public class Web_testign_HRM {
+import java.lang.System;
+import static java.lang.System.out;
+public class Web_testing_HRM {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
