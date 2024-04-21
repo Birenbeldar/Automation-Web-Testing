11c291330af6d218127f495981d00a178dffa04d
Birenbeldar <birenbeldar007@gmail.com>
2024-04-21 11:15:15 +0530

    new switch case program

--- Web_Automation_HRM/src/Loops_practicals/For_loop_practical_001.java
+++ Web_Automation_HRM/src/Loops_practicals/For_loop_practical_001.java
@@ -1,5 +1,14 @@
 package Loops_practicals;
 
 public class For_loop_practical_001 {
-
+    public static void main (String[]args){
+        for (int i = 1; i < 10 ;i++) {
+            if(i==5) {
+                System.out.println("I am Biren.This is " + i + " Time");
+                break;
+            }
+            System.out.println("I am Biren");
+        }
+        System.out.println("This is end");
+    }
 }
